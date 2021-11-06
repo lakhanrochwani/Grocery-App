9280422372cc671e23678c9b9189452c82126879
lakhanrochwani <rochwanilakhan@gmail.com>
2021-11-05 19:13:04 -0500

    1 file modified

--- src/GroceryApp.js
+++ src/GroceryApp.js
@@ -5,11 +5,9 @@ import './style.css';
 
 const Product = ({ name, votes, index, onVote }) => {
   const plus = () => {
-    // Call props.onVote to increase the vote count for this product
     onVote(1, index);
   };
   const minus = () => {
-    // Call props.onVote to decrease the vote count for this product
     onVote(-1, index);
   };
   return (
@@ -21,15 +19,13 @@ const Product = ({ name, votes, index, onVote }) => {
 };
 
 export default class GroceryApp extends React.Component {
-  // Finish writing the GroceryApp class
   state = { products: this.props.products };
 
   onVote = (dir, index) => {
-    // Update the products array accordingly ...
     this.setState(
       this.state.products.map((product, i) => {
         if (i === index) {
-          product.votes+=dir
+          product.votes += dir;
         }
         return product;
       })
@@ -39,18 +35,15 @@ export default class GroceryApp extends React.Component {
   render() {
     return (
       <ul>
-        {
-          /* Render an array of products, which should call this.onVote when + or - is clicked */
-          this.state.products.map((product, index) => (
-            <Product
-              index={index}
-              onVote={this.onVote}
-              key={product.name}
-              name={product.name}
-              votes={product.votes}
-            />
-          ))
-        }
+        {this.state.products.map((product, index) => (
+          <Product
+            index={index}
+            onVote={this.onVote}
+            key={product.name}
+            name={product.name}
+            votes={product.votes}
+          />
+        ))}
       </ul>
     );
   }
